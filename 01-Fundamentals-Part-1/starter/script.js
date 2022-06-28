aae3f0bebf2dd45764405565aae08fd6e6d4da6e
Łukasz Brodowski <lukaszbrodowski15@gmail.com>
2022-06-28 08:31:29 +0200

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,1 +1,44 @@
-alert ('lol');
+let js = 'mamma mia';
+
+    console.log(js);
+
+
+let testNameMamaMiaLala = "to wielbąd bo chodzi że góra że garb";
+let snake = first_name_mama_mia_lala = 'to snake czyli wąż bo sie wije że góra dół co nie???'
+    console.log(testNameMamaMiaLala);
+    console.log(testNameMamaMiaLala);
+    console.log(snake)
+    console.log('alt + shift mili panstwo');
+
+let PI = Math.PI;
+    console.log(PI);
+
+// wow  coment
+
+console.log(true);
+
+let me = false;
+console.log(typeof me);
+console.log(typeof 'me');
+
+let unde;
+console.log(unde)
+
+const mama = 'mama';
+
+console.log(mama);
+
+const currentYear = 2032;
+
+const ageJonas = currentYear - 1991;
+const ageSana = currentYear - 2018;
+console.log(ageJonas, ageSana);
+
+console.log(ageJonas * 2, ageSana / 2);
+
+let a, b;
+a = b = 10 - 5;
+console.log(a, b);
+
+const averageAge = (ageJonas + ageSana) / 2;
+console.log(ageJonas, ageSana, averageAge);
